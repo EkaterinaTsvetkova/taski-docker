358d0f4bd65789c5eb3baca215ca8e8585991664
EkaterinaTsvetkova <ekaterina.tsvetkova88@yandex.ru>
2025-02-16 02:11:50 +0300

    fix pep8

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,15 +1,16 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.160.51', 'https://practicumhost.hopto.org']
-
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 '84.201.160.51',
+                 'https://practicumhost.hopto.org']
 
 # Application definition
 
